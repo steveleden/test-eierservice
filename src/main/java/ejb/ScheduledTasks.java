ccabe717556eb4b0618edff5c12411e3c4a04b99
Stefan Leden <s.leden@hotmail.com>
2017-01-21 13:53:45 +0100

    Guthaben einbauen 2

--- src/main/java/ejb/ScheduledTasks.java
+++ src/main/java/ejb/ScheduledTasks.java
@@ -9,7 +9,7 @@ public class ScheduledTasks {
 	//Damit Schedule funktioniert, muss der Server mit der Configuration standalone-preview.xml gestartet werden.
 	//Auf Server doppelklicken, 'Runtime environment' anklicken, Configuration file auswaehlen.
 	//Falls beim Starten des Servers wegen des Timers ein Error erzeugt wird,
-	//Files in JBOSS_HOME/standalone/data/time-service-data löschen
+	//Files in JBOSS_HOME/standalone/data/time-service-data loeschen
 //	@Schedule(second = "*/10", minute = "*", hour = "*", persistent=false)
 //	public void someTask() {
 //		//System.out.println("EggStore: Test scheduler");
